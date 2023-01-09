902797c673246245b351310c18fab3114cb713d3
João Victor Barbosa <joabarbosa@REDE-EMPRO.empro.com.br>
2023-01-09 16:07:16 -0300

    Organizando o projeto.

---
+++ js/controllers/listaTelefonicaCtrl.js
@@ -0,0 +1,48 @@
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, $http) {
+    $scope.app = "Lista Telefonica";
+    $scope.contatos = [];
+    $scope.operadoras = [];
+
+    var linkContatos = "/angular/back/contatos.php";
+    var linkOperadoras = "/angular/back/operadoras.php";      
+
+    var carregarContatos = function () {
+        $http.get(linkContatos).success(function (data) {
+            $scope.contatos = data;
+        }).error(function (data, status) {
+            $scope.message = "Aconteceu um problema: " + data;
+        });
+    };
+
+    var carregarOperadoras = function () {
+        $http.get(linkOperadoras).success(function (data) {
+            $scope.operadoras = data;
+        });
+    };
+
+    $scope.adicionarContato = function (contato) {
+        contato.data = new Date();
+        $http.post(linkContatos, contato).success(function (data) {
+            delete $scope.contato;
+            $scope.contatoForm.$setPristine();
+            $scope.contatos.push(data);
+        });
+    };
+    $scope.apagarContatos = function (contatos) {
+        $scope.contatos = contatos.filter(function (contato) {
+            if (!contato.selecionado) return contato;
+        });
+    };
+    $scope.isContatoSelecionado = function (contatos) {
+        return contatos.some(function (contato) {
+            return contato.selecionado;
+        });
+    };
+    $scope.ordenarPor = function (campo) {
+        $scope.criterioDeOrdenacao = campo;
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+    };
+
+    carregarContatos();
+    carregarOperadoras();
+});
