8a2a3c176e705e396ffe69fc4d461af65e1e5fed
João Victor Barbosa <joabarbosa@REDE-EMPRO.empro.com.br>
2023-01-12 14:09:04 -0300

    Transformando as páginas da aplicação.

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -1,5 +1,11 @@
 angular.module("listaTelefonica").config(function ($routeProvider) {
     $routeProvider.when("/contatos", {
-        templateUrl = "view/contatos.html"
+        templateUrl: "view/contatos.html",
+        controller: "listaTelefonicaCtrl"
     });
+    
+    $routeProvider.when("/novoContato", {
+        templateUrl: "view/novoContato.html",
+        controller: "listaTelefonicaCtrl"
+    })
 });
